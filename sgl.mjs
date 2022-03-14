1226e3067561f0a96d1fe26ac83c68369c1c758d
Color <jgstringer@gmail.com>
2022-03-14 01:09:26 -0700

    add capsule image

--- sgl.mjs
+++ sgl.mjs
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
-const key = "YOUR_KEY_HERE";
-const steamId = "STEAM_ID_HERE";
+const key = "SECRET";
+const steamId = ""; // 76561198139366501 is mine if you wanna be my friend :)
 
 const payload = `{"steamid":${steamId},"include_appinfo":true}`;
 const request = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${key}&input_json=${payload}`;
@@ -12,7 +12,7 @@ fetch( request )
 	.then((data) => {
 		data.response.games.forEach( game => { 
 			console.log()
-			console.log(`<li><img src="http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg" /> ${game.name} -- played: ${game.playtime_forever ? 'yes' : 'no'}`);
+			console.log(`<li><img src="https://cdn.cloudflare.steamstatic.com/steam/apps/${game.appid}/capsule_231x87.jpg" /><img src="http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg" /> ${game.name} -- played: ${game.playtime_forever ? 'yes' : 'no'}`);
 		});
 	});
 console.log(`</ul></body></html>`)
