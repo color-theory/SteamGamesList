8cd1d454115b437cd137d63b1a1433e373300b41
color <jgstringer@gmail.com>
2026-05-16 15:36:18 -0500

    only save json. use env files

--- sgl.mjs
+++ sgl.mjs
@@ -1,18 +1,15 @@
 import fetch from "node-fetch";
+import { writeFileSync } from "fs";
+import "dotenv/config";
 
-const key = "SECRET";
-const steamId = ""; // 76561198139366501 is mine if you wanna be my friend :)
+const key = process.env.STEAM_API_KEY;
+const steamId = process.env.STEAM_ID;
 
 const payload = `{"steamid":${steamId},"include_appinfo":true}`;
 const request = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${key}&input_json=${payload}`;
 
-console.log(`<html><body><ul>`);
-fetch( request )
-	.then(response => response.json())
-	.then((data) => {
-		data.response.games.forEach( game => { 
-			console.log()
-			console.log(`<li><img src="https://cdn.cloudflare.steamstatic.com/steam/apps/${game.appid}/capsule_231x87.jpg" /><img src="http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg" /> ${game.name} -- played: ${game.playtime_forever ? 'yes' : 'no'}`);
-		});
-	});
-console.log(`</ul></body></html>`)
+const response = await fetch(request);
+const data = await response.json();
+
+writeFileSync("steam.json", JSON.stringify(data.response, null, 2));
+console.log(`Wrote ${data.response.games.length} games to steam.json`);
